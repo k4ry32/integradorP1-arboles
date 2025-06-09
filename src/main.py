d71fed5796a3d9162cc0a5a73a3d6d4a5ebd57c7
Karina <karisdext@gmail.com>
2025-06-09 01:26:55 -0300

    se agrega utilidad para popular abol, se corrigen detalles del main

--- src/main.py
+++ src/main.py
@@ -1,15 +1,18 @@
-from lib.ArbolBinario import ArbolBinario
+# Modulos
 from lib.Persona import Persona
+from lib.ArbolBinario import ArbolBinario
+from utils.popularArbol import popularArbol
 
 def menu():
     while True:
         print("1- Agregar una persona")
         print("2- Mostrar Arbol de Apellidos y Lista de Personas")
         print("3- Borrar una persona")
-        print("4- Salir")
+        print("4- Buscar una persona")
+        print("5- Salir")
         
         try:
-            opcion=int(input("Ingrese un numero: "))
+            opcion = int(input("\nIngrese una opcion: "))
         except ValueError:
             print("Opcion invalida")
             continue
@@ -18,32 +21,45 @@ def menu():
     return opcion
 
 def main():
-    opcion=0
+    arbol = ArbolBinario()
+    opcion = 0
 
-    arbol=ArbolBinario()
+    print("\n--------------------------------------------------------")
+    print("Bienvenido al arbol de apellidos!")
+    populate = input("Desea popular el arbol con personas de prueba? (si/no): ")
+    if populate.lower() == "si": popularArbol(arbol)
 
-    while opcion!=4:
-        opcion=menu()
+    while opcion != 5:
+        print("\n--------------------------------------------------------")
+        opcion = menu()
+        print("--------------------------------------------------------\n")
         
         match(opcion):
             case 1:
-                apellido=input("Ingrese un apellido: ")
-                nombre=input("Ingrese un nombre: ")
-                dni=input("Ingrese un DNI: ")
+                apellido = input("Ingrese el apellido: ")
+                nombre = input("Ingrese el nombre: ")
+                dni = input("Ingrese el DNI: ")
 
-                nuevaPersona=Persona(apellido,nombre,dni)
+                nuevaPersona = Persona(apellido, nombre, dni)
                 arbol.agregarPersona(nuevaPersona)
                 
             case 2:
-                arbol.mostrar()
+                print(arbol)
 
             case 3:
-                arbol.borrarPersona(arbol.raiz)
-
+                apellido = input("Ingrese el apellido de la persona que desea borrar: ")
+                # Agregar funcion para borrar persona
+                
             case 4:
+                apellido = input("Ingrese el apellido de la persona que desea buscar: ")
+                #  Agregar funcion para buscar persona
+
+            case 5:
+                print("Saliendo del programa...")
                 break
+
             case _:
-                print("Opcion invaida")
+                print("Opcion invalida")
                 pass
 
 main()
